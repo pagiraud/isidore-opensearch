fd613eed72869302b4379facbb4949ce4ef842ba
pagiraud <hyogapag@yahoo.fr>
2016-09-12 00:09:48 +0200

    Changement de l\'url de l\'application

--- isidore-plugins.py
+++ isidore-plugins.py
@@ -82,10 +82,6 @@ for resultat in root[1]:
 		description = "Aucune description disponible."
 	else:
 		description = description.replace('&','&amp;')
-#Code temporaire, tant que le bug de Virtuoso n'est pas résolu sur Isidore.
-#	titre = titre.encode('latin-1').decode('utf_8')
-#	description = description.encode('latin-1').decode('utf_8')
-#Fin du code temporaire
 	adresse = resultat[3][0].text
 	logo = resultat[4][0].text
 	os_shortname = titre[0:16] + "via ISIDORE"
@@ -122,7 +118,7 @@ for resultat in root[1]:
 	title = ancre + "<h1>" + titre + "</h1>\n"
 #	title = "<h1%s>%s</h1>\n" % (ancre,titre)
 	description = "<li class=\"description\">" + description + "</li>\n"
-	lien = "<li class=\"lien_os\"><button onclick=\"javascript:window.external.AddSearchProvider('http://www.insolit.org/isidore/" + os_filename + "')\" type=\"button\">Installez le plugin de recherche " + os_shortname + "</button></li>\n"
+	lien = "<li class=\"lien_os\"><button onclick=\"javascript:window.external.AddSearchProvider('http://www.insolit.org/isidore-opensearch/" + os_filename + "')\" type=\"button\">Installez le plugin de recherche " + os_shortname + "</button></li>\n"
 	logo = "<li class=\"logo\"><img alt=\"Image indisponible\" src=\"" + logo + "\" /></li>\n"
 	collec = "<div class=\"collec\">" + title + "<ul>" + description + lien + logo + "</ul></div><hr />"
 	print>>html,collec
